35aa85e328151510db6e7088fcce4ddc9b4fe89a
Vinicius De Martin Viude <demartinvinicius@gmail.com>
2018-10-22 13:20:22 -0300

    Revert "Versão 0.0.6"
    This reverts commit 9925586d09d39b5dab31a0e1bc7a9acbdc1f4d05.

--- automata.php
+++ automata.php
@@ -15,7 +15,7 @@ include('LIB_parse.php');
 // Bibliotecas Spreadsheet
 require 'vendor/autoload.php';
 
-syslog(LOG_INFO, "PHP Automata - Executando Automata");
+syslog(LOG_INFO, "Executando Automata");
 
 // Aceitar a politica de segurança da informação
 $data_array['sn-preauth-proceed'] = "Aceitar";
@@ -45,7 +45,7 @@ if(strstr($response_lg['FILE'],"There are already other user sessions in progres
     echo "\n";
     echo "Já ha uma sessão em andamento";
     echo "\n";
-    syslog(LOG_INFO,"PHP Automata - Saindo por sessão em andamento");
+    syslog(LOG_INFO,"Automata - Saindo por sessão em andamento");
     return;
 }
 
@@ -131,20 +131,6 @@ $leitor = \PhpOffice\PhpSpreadsheet\IOFactory::createReaderForFile("./teste.xls"
 $leitor->setReadDataOnly(true);
 $spreadsheet = $leitor->load("./teste.xls");
 
-$valor = $spreadsheet->getActiveSheet()->getCell('A1');
-echo "**************************\n";
-echo "Valor da celula\n";
-echo $valor;
-echo "\n******************************\n";
-
-if ($valor!='Atendimento') {
-    syslog(LOG_INFO, "PHP Automata - Erro na plailha recebida!");
-    logout();
-    return;
-}
-
-syslog(LOG_INFO,"PHP Automata - Planilha Ok");
-
 $escreve = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet);
 $escreve->setDelimiter(';');
 $escreve->setEnclosure('"');
@@ -183,149 +169,30 @@ if($result_hash->num_rows>0) {
     echo "\n";
     if ($hash_atual == hash_file("sha256","./teste.csv")) {
         echo "\nArquivo sem modificação não será inserido\n";
-        syslog(LOG_INFO,"PHP Automata - Sem modificação - Oferta Balcão");
+        $result_hash->close();
+        syslog(LOG_INFO,"Sem modificação");
+        logout();
+        return;
     }
-    else {
-        $sql_insert_hash = "insert into tb_env_envios (env_hash,env_dataenvio) values ('" .
-                            hash_file("sha256","./teste.csv") . "',now())";
-
-        $mysql_conn->query($sql_insert_hash);
-
-        if ($mysql_conn->query($sql)) {
-            echo "\n";
-            echo 'Valores inseridos na tabela';
-            echo "\n";
-        } else {
-            echo $mysql_conn->error;
-        }
-        syslog(LOG_INFO,"PHP Automata - Tabela atualizada");
-        
-    }
-    
-    
 }
+
+
 $result_hash->close();
 
-echo "\n*******************************************\n";
-echo "Iniciando a consulta empresa\n";
-echo "*********************************************\n";
-
-// Iniciando a consulta empresa.....
-//Acessando módulo de consulta
-$dados_consulta_emp['Controle']=3;
-$dados_consulta_emp['Processo']='Resultado - Oferta - Analítico';
-$dados_consulta_emp['Titulo']="";
-$dados_consulta_emp['Visao']=183;
-$dados_consulta_emp['Xml']='<Busca><Dados Colunas="43,88,96,46,47,12,103,2,105,44" Tipo="0" Esco="103" Atend="9" PerDe="01/01/2018" PerAte="31/12/2018"></Dados></Busca>';
-
-$resposta_consulta_emp = http("https://entrada.sesisenaisp.org.br/Consultas/,DanaInfo=sgset.sp.senai.br+Resultado.aspx",
-                              "https://entrada.sesisenaisp.org.br/dana-na/auth/url_0/login.cgi",
-                              "GET"
-                              ,$dados_consulta_emp,
-                              INCL_HEAD);
 
 
-echo "***********************************" . "\n" . "Resposta consulta" . "\n" . "***********************" . "\n";
-var_dump($resposta_consulta_emp);
-echo "\n";
-echo "\n";
+$sql_insert_hash = "insert into tb_env_envios (env_hash,env_dataenvio) values ('" .
+        hash_file("sha256","./teste.csv") . "',now())";
 
+$mysql_conn->query($sql_insert_hash);
 
-$dados_export_emp['valor']=1;
-$dados_export_emp['path']='testegui.xls';
-
-$resposta_export_emp = http("https://entrada.sesisenaisp.org.br/Consultas/,DanaInfo=sgset.sp.senai.br+ExportarResultado.aspx",
-                            "https://entrada.sesisenaisp.org.br/dana-na/auth/url_0/login.cgi",
-                            "POST",$dados_export,EXCL_HEAD);
-
-$hndfile_emp = fopen("./testegui.xls","wb");
-fwrite($hndfile_emp,$resposta_export_emp['FILE']);
-fclose($hndfile_emp);
-
-
-$leitor_emp = \PhpOffice\PhpSpreadsheet\IOFactory::createReaderForFile("./testegui.xls");
-$leitor_emp->setReadDataOnly(true);
-$spreadsheet_emp = $leitor->load("./testegui.xls");
-
-
-
-
-
-$escreve_emp = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet_emp);
-$escreve_emp->setDelimiter(';');
-$escreve_emp->setEnclosure('"');
-$escreve_emp->setLineEnding("\n");
-$escreve_emp->save("./testegui.csv");
-
-$valor_emp = $spreadsheet_emp->getActiveSheet()->getCell('B1');
-echo "**************************\n";
-echo "Valor da celula empresa\n";
-echo $valor_emp;
-echo "\n******************************\n";
-
-if ($valor_emp!='Situação da Proposta') {
-    syslog(LOG_INFO, "PHP Automata - Erro na planilha empresa recebida!");
-    logout();
-    return;
-}
-
-$sql_emp = "load data local infile '/home/ubuntu/webbots/automata/testegui.csv' into table tb_cem_cursos_empresa character set UTF8 fields terminated by ';' optionally enclosed by '\"' ignore 1 lines (cem_proposta,cem_situacao,@val1,cem_tipo_cliente,cem_funcionario,cem_curso,cem_corporativo,cem_empresa,cem_cnpj,@data_prop) set cem_valor = cast(replace(replace(replace(@val1,'.','|'),',','.'),'|','') as DECIMAL(9,2)), cem_data_proposta = date_add('1899-12-30',interval @data_prop day), cem_data_cadastro=now(), en2_id = LAST_INSERT_ID();";
-$sql_ultimo_hash_emp = "select en2_hash from tb_env_envios_empresa order by en2_dataenvio desc limit 1;";
-
-$result_hash_emp = $mysql_conn->query($sql_ultimo_hash_emp);
-
-echo "\n";
-echo "Valor retornado na tabela envios";
-echo $result_hash_emp->num_rows;
-echo "\n";
-
-if($result_hash_emp->num_rows>0) {
-    $result_array_emp = $result_hash_emp->fetch_array();
-    $hash_atual_emp = $result_array_emp['en2_hash'];
+if ($mysql_conn->query($sql)) {
     echo "\n";
-    echo "Hash na tabela:\n";
-    echo $hash_atual_emp;
+    echo 'Valores inseridos na tabela';
     echo "\n";
-    if ($hash_atual_emp == hash_file("sha256","./testegui.csv")) {
-        echo "\nArquivo Empresa sem modificação não será inserido\n";
-        syslog(LOG_INFO,"PHP Automata - Sem modificação - Empresa");
-    }
-    else {
-        $sql_insert_hash_emp = "insert into tb_env_envios_empresa (en2_hash,en2_dataenvio) values ('" .
-                            hash_file("sha256","./testegui.csv") . "',now())";
-
-        $mysql_conn->query($sql_insert_hash_emp);
-
-        if ($mysql_conn->query($sql_emp)) {
-            echo "\n";
-            echo 'Valores inseridos na tabela empresa';
-            echo "\n";
-        } else {
-            echo $mysql_conn->error;
-        }
-        syslog(LOG_INFO,"PHP Automata - Tabela empresa atualizada");
-        
-    }
+} else {
+    echo $mysql_conn->error;
 }
-else {
-        $sql_insert_hash_emp = "insert into tb_env_envios_empresa (en2_hash,en2_dataenvio) values ('" .
-                            hash_file("sha256","./testegui.csv") . "',now())";
-
-        $mysql_conn->query($sql_insert_hash_emp);
-
-        if ($mysql_conn->query($sql_emp)) {
-            echo "\n";
-            echo 'Valores inseridos na tabela empresa';
-            echo "\n";
-        } else {
-            echo $mysql_conn->error;
-        }
-        syslog(LOG_INFO,"PHP Automata - Tabela empresa atualizada");
-    
-}
-$result_hash_emp->close();
-    
-
-
+syslog(LOG_INFO,"Tabela atualizada");
 // Logout
 logout();
